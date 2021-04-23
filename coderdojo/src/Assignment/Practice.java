3ba8b9b74d2a0af04cfbbddb39771e2b3d3dcace
Tenisha <1010tenisha@gmail.com>
2021-04-23 16:45:15 -0700

    commit

---
+++ coderdojo/src/Assignment/Practice.java
@@ -0,0 +1,40 @@
+package Assignment;
+
+public class Practice {
+// Else If practice//
+
+	// Greater than //
+
+	public void Greaterthan(int a, int b) {
+		// TODO Auto-generated method stub
+		a = 20;
+		b = 30;
+		if (a > b) {
+			System.out.println("a is greater than b");
+		} else {
+			System.out.println("b is greater than a");
+		}
+	}
+
+	// Less than //
+
+	public void Lessthan(int c, int d) {
+		// TODO Auto-generated method stub
+		if (c < d)
+			System.out.println("c is less than d");
+	}
+
+	// Equal to //
+
+	public void Equalto(int e, int f) {
+		// TODO Auto-generated method stub
+		if (e == f)
+			System.out.println("");
+	}
+
+	public static void main(String args[]) {
+		Practice compare = new Practice();
+		compare.Equalto(20, 20);
+		compare.Equalto(30, 50);
+	}
+}
